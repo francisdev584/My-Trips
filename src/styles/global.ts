aa644084cbe07248f373bc9c0e07719b08c25473
Francinildo Rodrigues <francis584@gmail.com>
2021-09-07 14:13:49 -0300

    Add CSS variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eeeeee;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -18,6 +30,17 @@ const GlobalStyles = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
+    background: var(--background);
+    color: var(--white);
+  }
+
+  p, a {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
   }
 `
 
